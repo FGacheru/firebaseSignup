ff4014744147d9c1658e1cf35860f3958e7cdaa1
FGacheru <gacheruf12@gmail.com>
2023-08-23 15:21:11 +0300

    Learned a crazy thing ng add @angular/fire connects with firebase very fast

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'angularsignup001',
+    appId: '1:560016326737:web:d64a0e209ded9c3df53870',
+    databaseURL: 'https://angularsignup001-default-rtdb.firebaseio.com',
+    storageBucket: 'angularsignup001.appspot.com',
+    apiKey: 'AIzaSyDxAHROQ6jFieu5Mj5Gu2GKDxdP_5Jcnvg',
+    authDomain: 'angularsignup001.firebaseapp.com',
+    messagingSenderId: '560016326737',
+    measurementId: 'G-C4797H5FJQ',
+  },
+};
